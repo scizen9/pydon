d3e9ec1afc08e42726b815b67a911a4d891432f6
Don Neill <neilljd@gmail.com>
2022-08-25 15:23:46 -0700

    Handle keyboard interrupt in logtemps.py

--- kcwi/logtemps.py
+++ kcwi/logtemps.py
@@ -15,21 +15,25 @@ logging.basicConfig(format='%(asctime)s %(message)s',
 
 while True:
 
-    ktl_tempdet = ktl.cache('krds', 'TEMPDET')
-    tempdet = ktl_tempdet.read()
+    try:
+        ktl_tempdet = ktl.cache('krds', 'TEMPDET')
+        tempdet = ktl_tempdet.read()
 
-    ktl_temppkg = ktl.cache('krds', 'TEMPPKG')
-    temppkg = ktl_temppkg.read()
+        ktl_temppkg = ktl.cache('krds', 'TEMPPKG')
+        temppkg = ktl_temppkg.read()
 
-    # td = Red.tempdetr()
-    # tp = Red.temppkgr()
+        # td = Red.tempdetr()
+        # tp = Red.temppkgr()
 
-    ktl_pressure = ktl.cache('krvs', 'PRESSURE')
-    pressure = ktl_pressure.read()
+        ktl_pressure = ktl.cache('krvs', 'PRESSURE')
+        pressure = ktl_pressure.read()
 
-    ktl_current = ktl.cache('krvs', 'CURRENT')
-    current = ktl_current.read()
+        ktl_current = ktl.cache('krvs', 'CURRENT')
+        current = ktl_current.read()
 
-    logging.info("%s %s %s %s" % (tempdet, temppkg, pressure, current))
-    sys.stdout.flush()
-    time.sleep(30)
+        logging.info("%s %s %s %s" % (tempdet, temppkg, pressure, current))
+        sys.stdout.flush()
+        time.sleep(30)
+    except KeyboardInterrupt:
+        print("Exiting.")
+        sys.exit(0)
