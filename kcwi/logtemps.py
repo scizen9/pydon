eb5e059261162cbb663ec7a7e4917905d04fe3aa
Don Neill <neilljd@gmail.com>
2022-08-25 15:30:51 -0700

    Make date format compatible with dygraphs plots in logtemps.py

--- kcwi/logtemps.py
+++ kcwi/logtemps.py
@@ -7,7 +7,7 @@ import logging
 
 timestr = time.strftime("%Y%m%d-%H%M%S")
 logging.basicConfig(format='%(asctime)s %(message)s',
-                    level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S',
+                    level=logging.INFO, datefmt='%Y/%m/%d %H:%M:%S',
                     filename="dethk"+timestr+".log")
 
 
@@ -26,7 +26,7 @@ while True:
         # ktl_current = ktl.cache('krvs', 'CURRENT')
         # current = ktl_current.read()
 
-        logging.info("%s %s" % (tempdet, temppkg))  # , pressure, current))
+        logging.info(", %s, %s" % (tempdet, temppkg))  # , pressure, current))
         sys.stdout.flush()
         time.sleep(30)
     except KeyboardInterrupt:
