a08e902c2d5513bd2761a05afccf00a64d91369e
Don Neill <neilljd@gmail.com>
2022-08-25 14:27:16 -0700

    Added frameno and more logging

--- kcwi/day_begin.py
+++ kcwi/day_begin.py
@@ -1,16 +1,53 @@
+#!kpython
 import KCWI.Red as Red
 
 import ktl
 import time
-import sys
+import os
+import glob
 import logging
+import pyfits as pf
 
 timestr = time.strftime("%Y%m%d-%H%M%S")
 logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',
-    level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
+                    level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
+
+fspec = '/data/22????'
+dirlist = sorted([d for d in glob.glob(fspec) if os.path.isdir(d)],
+                 reverse=True)
+
+next_image_number = -1
+for d in dirlist:
+    ffspec = d + '/kr?????.fits'
+    flist = glob.glob(ffspec)
+    if not flist:
+        logging.info("No fits files in %s" % d)
+        continue
+    else:
+        flist = sorted(flist)
+        ff = pf.open(flist[-1])
+        current_image_number = ff[0].header['FRAMENO']
+        logging.info("Current FRAMENO: %d from %s" % (current_image_number,
+                                                      flist[-1]))
+        next_image_number = current_image_number + 1
+
+if next_image_number > 0:
+    logging.info("Setting FRAMENO to %d" % next_image_number)
+    ktl_frameno = ktl.cache('krds', 'FRAMENO')
+    ktl_frameno.write(next_image_number)
+else:
+    logging.warning("Could not set FRAMENO")
 
 outdir_str = '/data/' + time.strftime("%y%m%d")
+logging.info("Setting output directory to: %s" % outdir_str)
+
+if not os.path.isdir(outdir_str):
+    os.mkdir(outdir_str)
+    logging.info("Created directory: %s" % outdir_str)
+else:
+    logging.info("Directory already exists: %s" % outdir_str)
+
 ktl_outdir = ktl.cache('krds', 'OUTDIR')
 ktl_outdir.write(outdir_str)
 
-logging.info("Set output directory to: %s" % outdir_str)
+
