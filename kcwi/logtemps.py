00706266aa90ada3a39886f4788b1da743bf50c8
Don Neill <neilljd@gmail.com>
2022-08-25 15:17:19 -0700

    Remove Red.py library dependancies in logtemps.py

--- kcwi/logtemps.py
+++ kcwi/logtemps.py
@@ -1,6 +1,6 @@
 #!/kroot/rel/default/bin/kpython
 
-import KCWI.Red as Red
+# import KCWI.Red as Red
 
 import ktl
 import time
@@ -8,14 +8,21 @@ import sys
 import logging
 
 timestr = time.strftime("%Y%m%d-%H%M%S")
-logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',
+logging.basicConfig(format='%(asctime)s %(message)s',
                     level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S',
                     filename="dethk"+timestr+".log")
 
 
 while True:
-    td = Red.tempdetr()
-    tp = Red.temppkgr()
+
+    ktl_tempdet = ktl.cache('krds', 'TEMPDET')
+    tempdet = ktl_tempdet.read()
+
+    ktl_temppkg = ktl.cache('krds', 'TEMPPKG')
+    temppkg = ktl_temppkg.read()
+
+    # td = Red.tempdetr()
+    # tp = Red.temppkgr()
 
     ktl_pressure = ktl.cache('krvs', 'PRESSURE')
     pressure = ktl_pressure.read()
@@ -23,6 +30,6 @@ while True:
     ktl_current = ktl.cache('krvs', 'CURRENT')
     current = ktl_current.read()
 
-    logging.info("%s %s %s %s" % (td, tp, pressure, current))
+    logging.info("%s %s %s %s" % (tempdet, temppkg, pressure, current))
     sys.stdout.flush()
     time.sleep(30)
