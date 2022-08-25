3ea168dca91b98969bd6eaf13e60ad0e2a06bd21
Don Neill <neilljd@gmail.com>
2022-08-25 14:59:18 -0700

    Add pressure and current logging to logtemps.py

--- kcwi/logtemps.py
+++ kcwi/logtemps.py
@@ -2,6 +2,7 @@
 
 import KCWI.Red as Red
 
+import ktl
 import time
 import sys
 import logging
@@ -9,12 +10,19 @@ import logging
 timestr = time.strftime("%Y%m%d-%H%M%S")
 logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',
                     level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S',
-                    filename="tempdet"+timestr+".log")
+                    filename="dethk"+timestr+".log")
 
 
 while True:
     td = Red.tempdetr()
     tp = Red.temppkgr()
-    logging.info("%s %s" % (td, tp))
+
+    ktl_pressure = ktl.cache('krvs', 'PRESSURE')
+    pressure = ktl_pressure.read()
+
+    ktl_current = ktl.cache('krvs', 'CURRENT')
+    current = ktl_current.read()
+
+    logging.info("%s %s %s %s" % (td, tp, pressure, current))
     sys.stdout.flush()
     time.sleep(30)
