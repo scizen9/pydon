8989ddf10261aa1cdff7fc05c93d1281d4e1e64e
scizen9 <neilljd@gmail.com>
2022-08-30 11:10:34 -0700

    added logic to skip output if last file is less than 10 minutes old in check_ccd.py

--- kcwi/check_ccd.py
+++ kcwi/check_ccd.py
@@ -4,9 +4,10 @@ import ktl
 import time
 import os
 import sys
-from datetime import datetime
+from datetime import datetime, timedelta
 
 timestr = time.strftime("%Y-%m-%d %H:%M:%S")
+ten_minutes_ago = datetime.now() - timedelta(minutes=10)
 
 verbose = len(sys.argv) > 1
 
@@ -19,16 +20,25 @@ if len(output) == 1:
     ccdpower = int(ktl_ccdpower.read())
     # power is on
     if ccdpower == 1:
-        print(timestr + ": CCDPOWER ON!")
         # check last file
         ktl_loutfile = ktl.cache('krds', 'LOUTFILE')
         loutfile = ktl_loutfile.read()
         # loutfile is set
         if len(loutfile.strip()) > 0:
             ts = os.path.getmtime(loutfile)
-            ftime = datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-            print("Last file at " + ftime + ": " + loutfile)
+            ftime = datetime.fromtimestamp(ts)
+            
+            if ftime < ten_minutes_ago:
+                print(timestr + ": CCDPOWER ON!")
+                print("Last file at " + ftime.strftime('%Y-%m-%d %H:%M:%S')
+                        + ": " + loutfile)
+            else:
+                if verbose:
+                    print(timestr + ": CCDPOWER ON!")
+                    print("Last file at " + ftime.strftime('%Y-%m-%d %H:%M:%S')
+                            + ": " + loutfile)
         else:
+            print(timestr + ": CCDPWER ON!")
             print("Last file not recorded by server")
     else:
         if verbose:
