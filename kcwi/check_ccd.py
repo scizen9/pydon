1b3800f63df6055f89bce06f27cabdeced1c859b
Don Neill <neilljd@gmail.com>
2022-09-14 14:05:33 -0700

    Can now run check_ccd.py on kcwidev

--- kcwi/check_ccd.py
+++ kcwi/check_ccd.py
@@ -11,38 +11,36 @@ ten_minutes_ago = datetime.now() - timedelta(minutes=10)
 
 verbose = len(sys.argv) > 1
 
-# is krds running?
-stream = os.popen('pgrep -af krds_service')
-output = stream.readlines()
-# Yes, it's running
-if len(output) == 1:
+try:
     ktl_ccdpower = ktl.cache('krds', 'CCDPOWER')
-    ccdpower = int(ktl_ccdpower.read())
-    # power is on
-    if ccdpower == 1:
-        # check last file
-        ktl_loutfile = ktl.cache('krds', 'LOUTFILE')
-        loutfile = ktl_loutfile.read()
-        # loutfile is set
-        if len(loutfile.strip()) > 0:
-            ts = os.path.getmtime(loutfile)
-            ftime = datetime.fromtimestamp(ts)
-            
-            if ftime < ten_minutes_ago:
+except ktl.Exceptions.ktlError:
+    if verbose:
+        print(timestr + ': KRDS server not running!')
+    sys.exit(0)
+
+ccdpower = int(ktl_ccdpower.read())
+# power is on
+if ccdpower == 1:
+    # check last file
+    ktl_loutfile = ktl.cache('krds', 'LOUTFILE')
+    loutfile = ktl_loutfile.read()
+    # loutfile is set
+    if len(loutfile.strip()) > 0:
+        ts = os.path.getmtime(loutfile)
+        ftime = datetime.fromtimestamp(ts)
+
+        if ftime < ten_minutes_ago:
+            print(timestr + ": CCDPOWER ON!")
+            print("Last file at " + ftime.strftime('%Y-%m-%d %H:%M:%S')
+                  + ": " + loutfile)
+        else:
+            if verbose:
                 print(timestr + ": CCDPOWER ON!")
                 print("Last file at " + ftime.strftime('%Y-%m-%d %H:%M:%S')
-                        + ": " + loutfile)
-            else:
-                if verbose:
-                    print(timestr + ": CCDPOWER ON!")
-                    print("Last file at " + ftime.strftime('%Y-%m-%d %H:%M:%S')
-                            + ": " + loutfile)
-        else:
-            print(timestr + ": CCDPWER ON!")
-            print("Last file not recorded by server")
+                      + ": " + loutfile)
     else:
-        if verbose:
-            print(timestr + ": CCDPOWER OFF")
+        print(timestr + ": CCDPWER ON!")
+        print("Last file not recorded by server")
 else:
     if verbose:
-        print("KRDS not running")
+        print(timestr + ": CCDPOWER OFF")
