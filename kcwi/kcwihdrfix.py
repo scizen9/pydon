0dd4a6c4e9b2e4b8403b4ab5aa0ea8b4245b43b0
Don Neill <neilljd@gmail.com>
2022-11-29 11:03:21 -0800

    Initial Revision of kcwihdrfix.py

---
+++ kcwi/kcwihdrfix.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+from astropy.io import fits as pf
+import glob
+
+done = False
+
+while not done:
+    q = input("Filename number range (int, int): ")
+    if len(q) > 0:
+        s = q.split(',')
+        sfno = int(s[0])
+        if len(s) > 1:
+            efno = int(s[1]) + 1
+        else:
+            efno = sfno + 1
+        k = input("Keyword (str): ")
+        v = input("Value (str): ")
+        t = input("Type (str): ")
+        if len(t) > 0:
+            if 'int' in t:
+                vs = int(v)
+            elif 'float' in t:
+                vs = float(v)
+            elif 'str' in t:
+                vs = v
+            else:
+                print("Warning, unknown type: %s" % t)
+                vs = v
+        else:
+            vs = v
+
+        for fi in range(sfno, efno):
+            fspec = "%05d" % fi
+            flist = glob.glob("*%s.fits" % fspec)
+            if len(flist) == 1:
+                print(flist[0])
+                pf.setval(flist[0], k, value=vs)
+            elif len(flist) < 1:
+                print("no files found with fspec: *%s.fits" % fspec)
+            else:
+                print("too many files found with fspec: *%s.fits" % fspec)
+    else:
+        done = True
