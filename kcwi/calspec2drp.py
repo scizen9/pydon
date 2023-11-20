e8b524ad00c342a90176cf2fcc393ffae9194ebb
Don Neill <neilljd@gmail.com>
2023-11-20 14:04:41 -0800

    Use KCWI bandpass in calspec2drp.py

--- kcwi/calspec2drp.py
+++ kcwi/calspec2drp.py
@@ -27,24 +27,25 @@ def make_std(std_name, out_name):
     for i, w in enumerate(wave):
         if i > 0:
             dw[i] = wave[i] - wave[i-1]
-        if w < 11000:
+        if 3200 < w < 11000:
             idx.append(i)
 
+    w = wave[idx]
     f = flux[idx]
     d = dw[idx]
 
     print("Avg dw: %.3f A" % np.nanmean(d))
 
-    w0 = np.min(wave)
-    w1 = np.max(wave)
+    w0 = np.min(w)
+    w1 = np.max(w)
 
     pl.plot(wave, flux, 'x')
     pl.xlabel("Wavelength")
     pl.ylabel("Flux")
     pl.yscale("log")
-    pl.xlim(np.nanmin(wave), 11000)
+    pl.xlim(3200, 11000)
     pl.ylim(np.nanmin(f), np.nanmax(f))
-    pl.title(out_name + ': %.2f - %.2f A: %d pts' % (w0, w1, len(wave)))
+    pl.title(out_name + ': %.2f - %.2f A: %d pts' % (w0, w1, len(w)))
     pl.savefig(pout, bbox_inches="tight")
     pl.show()
 
